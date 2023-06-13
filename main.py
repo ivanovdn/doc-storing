793a9fe86b5441b6b9680d6876909d5b4fc0c6ac
Dmytro Ivanov <ivanoffdn@gmail.com>
2023-06-13 11:05:52 +0300

    changed load_docs and added establish_retriever

--- main.py
+++ main.py
@@ -68,23 +68,27 @@ def process_response(response, mode) -> dict:
     return ans
 
 
+@st.cache_data
+def load_docs(file_path: str):
+    # st.info("`Reading doc ...`")
+    if file_path.endswith(".pdf"):
+        doc = PyPDFLoader(file_path)
+        docs = doc.load_and_split(text_splitter=splitter)
+    return docs
+
+
 @st.cache_resource
 def establish_db():
     # st.info("`Chroma DB creating...`")
     emb = SentenceTransformerEmbeddings(model_name=config_dict["MODEL_NAME"])
     db = Chroma(embedding_function=emb, persist_directory=config_dict["DB_PATH"])
-    retriever = db.as_retriever()
-    return db, retriever
+    return db
 
 
-@st.cache_data
-def load_docs(file_path: str):
-    # st.info("`Reading doc ...`")
-    doc = PyPDFLoader(file_path)
-    docs = doc.load_and_split(text_splitter=splitter)
-    processed_docs = [preprocess(doc.page_content) for doc in docs]
-    metadata = [doc.metadata for doc in docs]
-    return processed_docs, metadata, docs
+@st.cache_resource
+def establish_retriever(_db):
+    retriever = _db.as_retriever()
+    return retriever
 
 
 @st.cache_resource
@@ -115,43 +119,47 @@ def define_summarization_chain(_llm):
 @st.cache_data
 def update_states_and_db(files: list, _db) -> None:
     for file_ in files:
-        # if file_.name not in st.session_state["uploaded_files"]:
-        st.session_state.uploaded_files.append(file_.name)
-        processed_docs, metadata, docs = load_docs(
-            f"{config_dict['FILE_PATH']}/{file_.name}"
-        )
+        file_name = file_.name
+        docs = load_docs(f"{config_dict['FILE_PATH']}/{file_name}")
+        processed_docs = [preprocess(doc.page_content) for doc in docs]
+        metadata = [doc.metadata for doc in docs]
         _db.add_texts(texts=processed_docs, metadatas=metadata)
         _db.persist()
-        return docs
+        st.session_state.uploaded_files[file_name] = docs
 
 
 def main():
-    # Sidebar
-    chromadb, retriever = establish_db()
+    # Establish main components
+    chromadb = establish_db()
+    retriever = establish_retriever(chromadb)
     llm = define_llm()
     summarization_chain = define_summarization_chain(llm)
-
-    st.sidebar.title("Menu")
-    mode = st.sidebar.selectbox("Select Mode", ["Search", "Chat", "Summary"])
-    if mode == "Search":
-        k_param = st.sidebar.radio("Select number of docs to return", [2, 3, 4, 5])
-    else:
-        k_param = 5
-    retriever.search_kwargs = {"k": k_param}
-
     qa_chain = chat(retriever, llm)
 
+    k_param = 5
+    retriever.search_kwargs = {"k": k_param}
+
     # Files
-    files = st.file_uploader(
-        "Upload Documents", type=["pdf"], accept_multiple_files=True
+    files_list = st.file_uploader(
+        "Upload Documents", type=["pdf", "jpg"], accept_multiple_files=True
     )
 
     if "uploaded_files" not in st.session_state:
-        st.session_state["uploaded_files"] = []
+        st.session_state["uploaded_files"] = {}
 
-    if files:
-        summary_docs = update_states_and_db(files, chromadb)
+    if files_list:
+        update_states_and_db(files_list, chromadb)
         st.write("Documents uploaded and processed.")
+
+    # Sidebar
+    st.sidebar.title("Menu")
+    mode = st.sidebar.selectbox("Select Mode", ["Search", "Chat", "Summary"])
+
+    if mode == "Summary":
+        dock_to_summarize = st.sidebar.radio(
+            "Pick dock to summarize", [file_.name for file_ in files_list]
+        )
+        print(st.session_state.uploaded_files[dock_to_summarize][:4])
 
     query = st.text_input(
         label=f"{mode}",
